7c9e9230c5bb1d4795ca5e44d2f1beefe0f48bbf
Daniel Wiesmann <daniel@tesselo.com>
2019-03-21 12:24:08 +0000

    Unindented over-indented code.

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,11 @@
 class TesselateMockResponseBase(object):
 
-        def json(self):
-            return {}
+    def json(self):
+        return {}
 
-        def raise_for_status(self):
-            pass
+    def raise_for_status(self):
+        pass
 
-        @property
-        def content(self):
-            return self.json()
+    @property
+    def content(self):
+        return self.json()
